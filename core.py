cbdf7e4be3f4f12c7fd80064c48572bc8df3f39e
kwugfighter <isaacli430@gmail.com>
2017-10-09 22:46:12 +0800

    first commit

---
+++ core.py
@@ -0,0 +1,26 @@
+import aiohttp, asyncio
+
+class Client:
+
+    def __init__(self):
+        pass
+
+    async def get_clan(self, tag):
+        pass
+
+    async def get_profile(self, tag):
+        pass
+
+class Profile:
+
+    __slots__ = ()
+
+    def __init__(self):
+        pass
+
+class Clan:
+
+    __slots__ = ()
+
+    def __init__(self):
+        pass
